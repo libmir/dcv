6d5d0c928c08a33350351c06c7be562c8e074ecb
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2016-01-29 22:17:23 +0100

    Implemented image I/O basics using imageformats library.

--- source/dcv/io/package.d
+++ source/dcv/io/package.d
@@ -2,12 +2,12 @@ module dcv.io;
 
 private import std.exception : enforce;
 private import std.range : array;
-private import std.algorithm.iteration : reduce;
-private import std.algorithm.iteration : map;
+private import std.algorithm : reduce, map;
 private import std.string : toLower;
 private import std.path : extension;
 
-public import dcv.core.image, mir.ndslice;
+public import mir.ndslice;
+public import dcv.core.image;
 
 private import imageformats;
 
@@ -17,7 +17,11 @@ struct ReadParams {
     BitDepth depth = BitDepth.BD_UNASSIGNED;
 }
 
-/// Read image from the file system.
+/** 
+ * Read image from the file system.
+ * 
+ * 
+*/
 Image imread(in string path,
     ReadParams params = ReadParams(ImageFormat.IF_UNASSIGNED, BitDepth.BD_UNASSIGNED)) {
     return imreadImpl_imageformats(path, params);
@@ -48,16 +52,15 @@ bool imwrite(size_t dims, T)(in string path, Slice!(dims, T*) slice) {
 	auto sdata = slice.reshape(slice.shape[].reduce!"a*b").array;
 
 	static if (is(T == ubyte)) {
-		imwrite(path, slice.shape[0], slice.shape[1], format, BitDepth.BD_8, sdata);
+		return imwrite(path, slice.shape[0], slice.shape[1], format, BitDepth.BD_8, sdata);
 	} else static if (is(T == ushort)) {
 		enforce(path.extension.toLower == ".png", "Writing 16-bit image has to be in PNG format.");
-		imwrite(path, slice.shape[0], slice.shape[1], format, BitDepth.BD_16, cast(ubyte[])sdata);
+		return imwrite(path, slice.shape[0], slice.shape[1], format, BitDepth.BD_16, cast(ubyte[])sdata);
 	} else static if (is (T == float)) {
 		throw new Exception("Writting image format not supported.");
 	} else {
 		throw new Exception("Writting image format not supported.");
 	}
-	return true;
 }
 
 private:
@@ -98,8 +101,4 @@ int imreadImpl_imageformats_adoptFormat(ImageFormat format) {
     else if (format == ImageFormat.IF_MONO_ALPHA)
         ch = ColFmt.YA;
     return ch;
-}
-
-void imreadImpl_adoptType(Image image, ReadParams params) {
-
 }
