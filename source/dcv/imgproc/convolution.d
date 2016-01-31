636fbf3311fe1ce4c7e62f0b7ade1d9f3c4e5869
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2016-02-01 00:31:50 +0100

    Implented basic 1D and 2D convolution.

--- source/dcv/imgproc/convolution.d
+++ source/dcv/imgproc/convolution.d
@@ -1,5 +1,9 @@
 ﻿module dcv.imgproc.convolution;
 
+private import dcv.core.memory;
+
+private import core.cpuid;
+private import core.simd;
 
 private import std.traits;
 private import std.range;
@@ -11,10 +15,13 @@ private import std.algorithm.comparison : max, min;
 private import std.exception : enforce;
 private	import std.parallelism;
 
+debug {
+	import std.stdio : writeln;
+}
 
 Slice!(N, V*) conv(V, K, size_t N, size_t NK)(Slice!(N, V*) range, Slice!(NK, K*) kernel, 
 	Slice!(N, V*) prealloc = Slice!(N, V*)(),
-	Slice!(N, V*) mask = Slice!(N, V*)()) 
+	Slice!(N, V*) mask = Slice!(N, V*)())
 {
 	static assert(isAssignable!(V, K), "Uncompatible types for range and kernel");
 	static if (N == 1) {
@@ -32,55 +39,175 @@ Slice!(N, V*) conv(V, K, size_t N, size_t NK)(Slice!(N, V*) range, Slice!(NK, K*
 	}
 }
 
+unittest {
+	
+	import std.numeric : normalize;
+
+	// test 1
+	auto r1 = [0., 1., 2., 3., 4., 5.].sliced(6);
+	auto k1 = [-1., 0., 1.].sliced(3);
+
+	auto res1 = r1.conv(k1);
+
+	assert(res1.equal([0., 2., 2., 2., 2., 0.]));
+
+	k1 = [0.3333, 0.3333, 0.3333].sliced(3);
+
+	auto res2 = r1.conv(k1);
+
+	assert(res2.equal([2. / 3., 1., 2., 3., 4., 13. / 3.]));
+
+}
+
 private:
 
+// TODO: implement SIMD
 Slice!(1, V*) conv1Impl(V, K)(Slice!(1, V*) range, Slice!(1, K*) kernel, Slice!(1, V*) prealloc) {
 
+	import std.math : abs, floor;
+
 	if (prealloc.empty || prealloc.shape != range.shape)
-		prealloc = new V[cast(ulong)range.shape.reduce!"a*b"].sliced(range.shape);
+		prealloc = uninitializedArray!(V[])(cast(ulong)range.length).sliced(range.shape);
 
-	enforce(&range[0] != &prealloc[0], "Preallocated range value has point to a different data from that of a input range.");
+	enforce(&range[0] != &prealloc[0], 
+		"Preallocated has to contain different data from that of a input range.");
 
-	if (kernel.length == 3) {
-		conv1Impl_3(range, kernel, prealloc);
-	} else {
-		int ks = cast(int)kernel.length; // kernel size
-		int kh = max(1, ks / 2);
-		for(int i = kh; i < range.length-kh; i++) {
-			real v = 0;
-			for(int j = -kh; j < kh+1; ++j) {
-				v += range[i+j]*kernel[j+kh];
+	auto rl = range.length;
+	int ks = cast(int)kernel.length; // kernel size
+	int kh = max(1, cast(int)(floor(cast(float)ks / 2.))); // kernel size half
+	int ke = cast(int)(ks % 2 == 0 ? kh-1 : kh);
+	int rt = cast(int)(ks % 2 == 0 ? rl - 1 - kh : rl - kh); // range top
+
+	// run main (inner) loop
+	foreach(i; kh.iota(rt).parallel) {
+		V v = 0;
+		V *rp = &range[i];
+		for(int j = -kh; j < ke+1; ++j) {
+			v += rp[j]*kernel[j+kh];
+		}
+		prealloc[i] = v;
+	}
+
+	// run starting edge with mirror (symmetric) indexing.
+	foreach(i; 0 .. kh) {
+		V v_start = 0;
+		for(int j = -kh; j < ke+1; ++j) {
+			v_start += range[abs(i+j)]*kernel[j+kh];
+		}
+		prealloc[i] = v_start;
+	}
+
+	// run ending edge with mirror (symmetric) indexing.
+	foreach(i; rt .. rl) {
+		V v_end = 0;
+		for(int j = -kh; j < ke+1; ++j) {
+			v_end += range[(rl-1)-abs(j)]*kernel[j+kh];
+		}
+		prealloc[i] = v_end;
+	}
+	return prealloc;
+}
+
+Slice!(2, V*) conv2Impl(V, K)(Slice!(2, V*) range, Slice!(2, K*) kernel, Slice!(2, V*) prealloc) {
+
+	import std.math : abs, floor;
+
+	if (prealloc.empty || prealloc.shape != range.shape)
+		prealloc = uninitializedArray!(V[])(cast(ulong)range.shape.reduce!"a*b").sliced(range.shape);
+
+	enforce(&range[0, 0] != &prealloc[0, 0], 
+		"Preallocated has to contain different data from that of a input range.");
+
+	auto rr = range.length!0; // range rows
+	auto rc = range.length!1; // range columns
+
+	int krs = cast(int)kernel.length!0; // kernel rows
+	int kcs = cast(int)kernel.length!1; // kernel rows
+
+	int krh = max(1, cast(int)(floor(cast(float)krs / 2.))); // kernel rows size half
+	int kch = max(1, cast(int)(floor(cast(float)kcs / 2.))); // kernel rows size half
+
+	int kre = cast(int)(krs % 2 == 0 ? krh-1 : krh);
+	int kce = cast(int)(kcs % 2 == 0 ? kch-1 : kch);
+
+	int rrt = cast(int)(krs % 2 == 0 ? rr - 1 - krh : rr - krh); // range top
+	int rct = cast(int)(kcs % 2 == 0 ? rc - 1 - kch : rc - kch); // range top
+
+	// run inner body convolution of the matrix.
+	foreach(i; krh.iota(rrt).parallel) {
+		auto row = prealloc[i, 0..rc];
+		foreach(j; kch.iota(rct)) {
+			V v = 0;
+			for(int ii = -krh; ii < krh+1; ++ii) {
+				for(int jj = -kch; jj < kch+1; ++jj) {
+					v += range[i+ii, j+jj]*kernel[ii+krh, jj+kch];
+				}
 			}
-			prealloc[i] = v;
+			row[j] = v;
 		}
 	}
 
+	 // run upper edge with mirror (symmetric) indexing.
+	 auto row = prealloc[0, 0..rc];
+	 foreach(j; 0.iota(rc).parallel) {
+	 V v = 0;
+	 for(int ii = -krh; ii < krh+1; ++ii) {
+	 for(int jj = -kch; jj < kch+1; ++jj) {
+	 immutable jjj = j+jj;
+	 immutable jj_pos = jjj < 0 ? abs(jjj) :	jjj > rc-1 ? rc-1-abs(jj) : jjj;
+	 v += range[abs(ii), jj_pos]*kernel[ii+krh, jj+kch];
+	 }
+	 }
+	 row[j] = v;
+	 }
+
+	 // run lower edge with mirror (symmetric) indexing.
+	 row = prealloc[rr-1, 0..rc];
+	 foreach(j; 0.iota(rc).parallel) {
+	 V v = 0;
+	 for(int ii = -krh; ii < krh+1; ++ii) {
+	 for(int jj = -kch; jj < kch+1; ++jj) {
+	 immutable jjj = j+jj;
+	 immutable jj_pos = jjj < 0 ? abs(jjj) :	jjj > rc-1 ? rc-1-abs(jj) : jjj;
+	 v += range[(rr-1)-abs(ii), jj_pos]*kernel[ii+krh, jj+kch];
+	 }
+	 }
+	 row[j] = v;
+	 }
+
+	 // run left edge with mirror (symmetric) indexing.
+	 auto col = prealloc[0..rr, 0];
+	 foreach(i; 0.iota(rr).parallel) {
+	 V v = 0;
+	 for(int ii = -krh; ii < krh+1; ++ii) {
+	 for(int jj = -kch; jj < kch+1; ++jj) {
+	 immutable iii = i+ii;
+	 immutable ii_pos = iii < 0 ? abs(iii) :	iii > rr - 1 ? rr-1-abs(ii) : iii;
+	 v += range[ii_pos, abs(jj)]*kernel[ii+krh, jj+kch];
+	 }
+	 }
+	 col[i] = v;
+	 }
+
+	 // run right edge with mirror (symmetric) indexing.
+	 col = prealloc[0..rr, rc-1];
+	 foreach(i; 0.iota(rr).parallel) {
+	 V v = 0;
+	 for(int ii = -krh; ii < krh+1; ++ii) {
+	 for(int jj = -kch; jj < kch+1; ++jj) {
+	 immutable iii = i+ii;
+	 immutable ii_pos = iii < 0 ? abs(iii) :	iii > rr - 1 ? rr-1-abs(ii) : iii;
+	 v += range[ii_pos, (rc-1)-abs(jj)]*kernel[ii+krh, jj+kch];
+	 }
+	 }
+	 col[i] = v;
+	 }
+
 	return prealloc;
 }
 
-Slice!(1, V*) conv1Impl_3(V, K)(Slice!(1, V*) range, Slice!(1, K*) kernel, Slice!(1, V*) prealloc) {
-
-	import core.simd;
-	import core.cpuid;
-
-	foreach(i, ref e; taskPool.parallel(range)) {
-		if (i == 0 || i == range.length-1)
-			continue;
-	//foreach(i; 1.iota(range.length-1)) {
-	//for(int i = 1; i < range.length-1; ++i) {
-		prealloc[i] = range[i-1]*kernel[0];
-		prealloc[i] += range[i]*kernel[1];
-		prealloc[i] += range[i+1]*kernel[2];
-	}
-
-	return prealloc;
-}
-
-Slice!(2, V*) conv2Impl(V, K)(Slice!(2, V*) range, Slice!(2, K*) kernel, Slice!(1, V*) prealloc) {
-	return range;
-}
-
-Slice!(3, V*) conv3Impl(V, K, size_t NK)(Slice!(3, V*) range, Slice!(NK, K*) kernel, Slice!(1, V*) prealloc) 
+Slice!(3, V*) conv3Impl(V, K, size_t NK)(Slice!(3, V*) range, Slice!(NK, K*) kernel, Slice!(1, V*) prealloc)
 {
 	return range;
 }
+
