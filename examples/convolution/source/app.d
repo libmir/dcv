9d99274302ad705f0bc883c433cb508f56bc70c2
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2016-02-02 22:09:30 +0100

    code reformat

--- examples/convolution/source/app.d
+++ examples/convolution/source/app.d
@@ -14,41 +14,40 @@ import dcv.io : imread, imwrite;
 import dcv.imgproc.convolution : conv;
 import dcv.imgproc.filter : gaussian;
 
-
 int main(string[] args) {
-	
+
     Image img = null;
-	string impath = "";
+    string impath = "";
 
     if (args.length < 2) {
-		impath = "../data/lena.png";
-    } else {
-		impath = args[1];
-	}
+        impath = "../data/lena.png";
+    }
+    else {
+        impath = args[1];
+    }
 
-	img = imread(impath); // read an image from filesystem.
+    img = imread(impath); // read an image from filesystem.
 
     if (img.empty) { // check if image is properly read.
         writeln("Cannot read image at: " ~ impath);
         return 1;
     }
 
-    Slice!(3, float*) imslice = img
-		.asType!float // convert Image data type from ubyte to float
-		.sliced!float; // slice image data - calls img.data!float.sliced(img.height, img.width, img.channels)
+    Slice!(3, float*) imslice = img.asType!float // convert Image data type from ubyte to float
+    .sliced!float; // slice image data - calls img.data!float.sliced(img.height, img.width, img.channels)
 
     // create gaussian convolution kernel
     auto kernel = gaussian!float(2, 5, 5);
 
-	StopWatch s;
+    StopWatch s;
 
-	writeln("Waiting for threads to be spawned and ready...");
-	Thread.getThis.sleep(dur!"msecs"(1000));
+    writeln("Waiting for threads to be spawned and ready...");
+    Thread.getThis.sleep(dur!"msecs"(1000));
 
-	s.start;
+    s.start;
     // perform convultionon gray image using average kernel
     auto blur = imslice.conv(kernel);
-	writeln("Convolution done in: ", s.peek.msecs, "ms");
+    writeln("Convolution done in: ", s.peek.msecs, "ms");
 
     /*
 	//Pre-allocation of the return buffer is allowed through third input parameter:
