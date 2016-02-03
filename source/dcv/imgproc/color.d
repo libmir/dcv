3131a472cbd6ddb91090c443d80db474d281b72e
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2016-02-03 23:17:40 +0100

    Code reformating.

--- source/dcv/imgproc/color.d
+++ source/dcv/imgproc/color.d
@@ -9,18 +9,23 @@ private import std.experimental.ndslice;
 private import dcv.core.utils;
 
 private import std.array : uninitializedArray;
-private import std.traits : CommonType, isFloatingPoint;
-private import std.algorithm.iteration : sum, each, reduce;
+private import std.traits : CommonType, isFloatingPoint, isAssignable;
+private import std.algorithm.iteration : sum, each, reduce, map;
+private import std.algorithm.mutation : copy;
 private import std.algorithm.comparison : equal;
-private import std.range : put;
+private import std.algorithm : swap;
+private import std.range : zip, array, iota;
+private import std.parallelism : parallel;
 
 /**
  * RGB to Grayscale convertion strategy.
  */
 enum Rgb2GrayConvertion : size_t {
-	MEAN, /// Mean the RGB values and assign to gray.
-	LUMINANCE_PRESERVE /// Use luminance preservation technique (0.2126R + 0.715G + 0.0722B). 
+	MEAN = 0, /// Mean the RGB values and assign to gray.
+	LUMINANCE_PRESERVE = 1 /// Use luminance preservation technique (0.2126R + 0.715G + 0.0722B). 
 }
+
+
 
 /**
  * Convert RGB image to grayscale.
@@ -39,36 +44,12 @@ enum Rgb2GrayConvertion : size_t {
  */
 Slice!(2, V*) rgb2gray(V)(Slice!(3, V*) range, 
 	Slice!(2, V*) prealloc = emptySlice!(2, V), 
-	Rgb2GrayConvertion conv = Rgb2GrayConvertion.LUMINANCE_PRESERVE) {
+	Rgb2GrayConvertion conv = Rgb2GrayConvertion.LUMINANCE_PRESERVE) pure nothrow @trusted {
 
-	if (prealloc.empty) {
-		if (!(range.shape[0..2][].equal(prealloc.shape[0..2][])))
-			prealloc = uninitializedArray!(V[])(range.shape[0]*range.shape[1])
-				.sliced(range.shape[0], range.shape[1]);
-	}
+	auto m = rgb2GrayMltp[conv].map!(a => cast(real)a).array;
+	m[] /= m.sum;
 
-	real [3] mltp; 
-
-	if (conv == Rgb2GrayConvertion.MEAN) {
-		mltp[] = 1.;
-	} else {
-		mltp[0] = 0.2126;
-		mltp[1] = 0.715;
-		mltp[2] = 0.0722;
-	}
-
-	auto s = mltp[].sum;
-	mltp[] /= s;
-
-	for (size_t r = 0; r < range.shape[0]; ++r) {
-		for (size_t c = 0; c < range.shape[1]; ++c) {
-			prealloc[r, c] = cast(V)(
-				range[r, c, 0]*mltp[0] +
-				range[r, c, 1]*mltp[1] +
-				range[r, c, 2]*mltp[2]);
-		}
-	}
-	return prealloc;
+	return rgb2grayImpl(range, prealloc, m);
 }
 
 /**
@@ -91,36 +72,13 @@ Slice!(2, V*) rgb2gray(V)(Slice!(3, V*) range,
  */
 Slice!(2, V*) bgr2gray(V)(Slice!(3, V*) range, 
 	Slice!(2, V*) prealloc = emptySlice!(2, V), 
-	Rgb2GrayConvertion conv = Rgb2GrayConvertion.LUMINANCE_PRESERVE) {
+	Rgb2GrayConvertion conv = Rgb2GrayConvertion.LUMINANCE_PRESERVE) pure nothrow @trusted {
 
-	// TODO: implement rgb2gray and bgr2gray as same function - organize better!
+	auto m = rgb2GrayMltp[conv].map!(a => cast(real)a).array;
+	m[] /= m.sum;
+	m[0].swap(m[2]);
 
-	if (prealloc.empty || (range.shape[0..2][]).equal(prealloc.shape[0..2][]))
-		prealloc = uninitializedArray!(V[])(range.shape.reduce!"a*b")
-			.sliced(range.shape[0], range.shape[1]);
-
-	real [3] mltp; 
-
-	if (conv == Rgb2GrayConvertion.MEAN) {
-		mltp[] = 1.;
-	} else {
-		mltp[0] = 0.2126;
-		mltp[1] = 0.715;
-		mltp[2] = 0.0722;
-	}
-
-	auto s = mltp[].sum;
-	mltp[] /= s;
-
-	for (size_t r = 0; r < range.shape[0]; ++r) {
-		for (size_t c = 0; c < range.shape[1]; ++c) {
-			prealloc[r, c] = cast(V)(
-				range[r, c, 0]*mltp[0] +
-				range[r, c, 1]*mltp[1] +
-				range[r, c, 2]*mltp[2]);
-		}
-	}
-	return prealloc;
+	return rgb2grayImpl(range, prealloc, m);
 }
 
 /**
@@ -140,13 +98,14 @@ Slice!(2, V*) bgr2gray(V)(Slice!(3, V*) range,
  * Returns RGB version of the given grayscale image.
  */
 Slice!(3, V*) gray2rgb(V)(Slice!(2, V*) range, 
-	Slice!(3, V*) prealloc = emptySlice!(2, V)) {
-	if (prealloc.empty || (range.shape[0..2][]).equal(prealloc.shape[0..2][]) || prealloc.shape[2] != 3)
-		prealloc = uninitializedArray!(V[])(range.shape.reduce!"a*b"*3)
-			.sliced(range.shape[0], range.shape[1], 3);
+	Slice!(3, V*) prealloc = emptySlice!(3, V)) pure nothrow @trusted {
 
-	for (size_t r = 0; r < range.shape[0]; ++r) {
-		for (size_t c = 0; c < range.shape[1]; ++c) {
+	if (prealloc.empty || (range.shape[0..2][]).equal(prealloc.shape[0..2][]) || prealloc.shape[2] != 3)
+		prealloc = uninitializedArray!(V[])(range.length!0*range.length!1*3)
+			.sliced(range.length!0, range.length!1, 3);
+
+	for (size_t r = 0; r < range.length!0; ++r) {
+		for (size_t c = 0; c < range.length!1; ++c) {
 			immutable v = range[r, c];
 			prealloc[r, c, 0] = v;
 			prealloc[r, c, 1] = v;
@@ -159,4 +118,50 @@ Slice!(3, V*) gray2rgb(V)(Slice!(2, V*) range,
 
 unittest {
 	// TODO: design the test...
+}
+
+
+private:
+
+immutable real [][] rgb2GrayMltp = [
+	[0.3333, 0.3333, 0.3333],
+	[0.2126, 0.715, 0.0722]
+];
+
+Slice!(2, V*) rgb2grayImpl(V)(Slice!(3, V*) range, 
+	Slice!(2, V*) prealloc, in real[] m) pure nothrow @trusted {
+	if (prealloc.empty) {
+		if (!(range.shape[0..2][].equal(prealloc.shape[0..2][])))
+			prealloc = uninitializedArray!(V[])(range.shape[0]*range.shape[1])
+				.sliced(range.shape[0], range.shape[1]);	
+	}
+
+	auto rp = range.pack!1;
+
+	auto rows = rp.length!0;
+	auto cols = rp.length!1;
+
+	for (size_t i = 0; i < rows; ++i) {
+		auto g_row = prealloc[i, 0..cols];
+		auto rgb_row = rp[i, 0..cols];
+		size_t j = 0;
+		auto rgb = rgb_row[0];
+		for (; j < cols; ++j) {
+			g_row[j] = cast(V) (
+				rgb[0]*m[0] +
+				rgb[1]*m[1] +
+				rgb[2]*m[2]
+				);
+		}
+	}
+
+	/*
+	// this is ~4x slower than nested foor loops?
+	range.pack!1
+		.byElement
+		.map!(rgb => cast(ubyte)(rgb[0]*m[0] + rgb[1]*m[1] + rgb[2]*m[2]))
+		.copy(prealloc.byElement);
+	*/
+
+	return prealloc;
 }
