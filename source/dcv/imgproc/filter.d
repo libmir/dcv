cd8f8a98bf5d2a63ecc66f77a4a0a49cb3d0144a
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2016-02-06 23:07:54 +0100

    cleanup gaussian implementation.

--- source/dcv/imgproc/filter.d
+++ source/dcv/imgproc/filter.d
@@ -31,24 +31,22 @@ auto gaussian(V)(real sigma, size_t width, size_t height) if (isNumeric!V) {
 	h = hg ./ sum(hg(:));
 	*/
 
-	immutable sqr = "a*a";
-
 	auto h = new V[width*height].sliced(height, width);
 
 	int arrv_w = -(cast(int)width-1)/2;
 	int arrv_h = -(cast(int)height-1)/2;
-	float sgm = 2*(sigma*sigma);
+	float sgm = 2*(sigma^^2);
 
 	foreach(r; 0..height) {
 		arrv_w.iota(-arrv_w+1)
-			.map!(e => cast(V)(e*e))
+			.map!(e => cast(V)(e^^2))
 			.array
 			.copy(h[r]);
 	}
 
 	foreach(c; 0..width) {
 		auto cadd = arrv_h.iota(-arrv_h+1)
-			.map!(e => cast(V)(e*e))
+			.map!(e => cast(V)(e^^2))
 			.array;
 		h[0..height, c][] += cadd[];
 		h[0..height, c].map!((ref v) => v = (-(v) / sgm).exp).copy(h[0..height, c]);
