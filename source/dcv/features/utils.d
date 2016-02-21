9ea54c0741003e3ba03dcad6280933d0c8f2f27d
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2016-02-22 00:31:42 +0100

    Added corner detection (harris and shi-tomasi) Added corner extraction methods.

---
+++ source/dcv/features/utils.d
@@ -0,0 +1,111 @@
+module dcv.features.utils;
+
+
+/**
+ * Module contains utilities common for all algorithms which operate with feature points.
+ */
+
+private import std.experimental.ndslice;
+private import std.traits : isNumeric;
+
+
+/**
+ * Extract corners as array of 2D points, from response matrix.
+ * 
+ * params:
+ * cornerResponse = Response matrix, collected as output from corner
+ * detection algoritms such as harrisCorners, or shiTomasiCorners.
+ * count = Number of corners which need to be extracted. Default is
+ * -1 which indicate that all responses with value above the threshold
+ * will be returned.
+ * threshold = Response threshold - response values in the matrix
+ * larger than this are considered as valid corners.
+ * 
+ * return:
+ * Dynamic array of ulong[2], as in array of 2D points, of corner reponses 
+ * which fit the given criteria.
+ */
+ulong [2][] extractCorners(T)(Slice!(2, T*) cornerResponse, 
+	long count = -1, T threshold = cast(T)0) @trusted pure nothrow
+if (isNumeric!T)
+{
+	import std.algorithm : sort, map, min;
+	import std.array : array;
+	import std.range : take, iota;
+
+	ulong [2] [float] features;
+
+	if (cornerResponse.empty) {
+		return typeof(return).init;
+	}
+
+	auto rows = cornerResponse.length!0;
+	auto cols = cornerResponse.length!1;
+
+	foreach(r; rows.iota) {
+		foreach(c; cols.iota) {
+			auto res = cornerResponse[r, c];
+			if (res > threshold)
+				features[res] = [r, c];
+		}
+	}
+
+	return sort!"a > b"(features.keys)
+			.take(count > 0 ? min(count, features.length) : features.length)
+			.map!(a => features[a])
+			.array;
+}
+
+unittest {
+	auto image = [
+		0., 0., 0.,
+		0., 1., 0.,
+		0., 0., 0.
+	].sliced(3, 3);
+
+	auto res = image.extractCorners;
+
+	assert(res.length == 1);
+	assert(res[0] == [1, 1]);
+}
+
+unittest {
+	auto image = [
+		0., 0.1, 0.,
+		0., 0.3, 0.,
+		0., 0.2, 0.
+	].sliced(3, 3);
+
+	auto res = image.extractCorners;
+
+	assert(res.length == 3);
+	assert(res[0] == [1, 1]);
+	assert(res[1] == [2, 1]);
+	assert(res[2] == [0, 1]);
+}
+
+unittest {
+	auto image = [
+		0., 0.1, 0.,
+		0., 0.3, 0.,
+		0., 0.2, 0.
+	].sliced(3, 3);
+
+	auto res = image.extractCorners(1);
+
+	assert(res.length == 1);
+	assert(res[0] == [1, 1]);
+}
+
+unittest {
+	auto image = [
+		0., 0.1, 0.,
+		0., 0.3, 0.,
+		0., 0.2, 0.
+	].sliced(3, 3);
+
+	auto res = image.extractCorners(-1, 0.2);
+
+	assert(res.length == 1);
+	assert(res[0] == [1, 1]);
+}
