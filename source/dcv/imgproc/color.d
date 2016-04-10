cc4c134052e3bc4013ce73da19911ae14e1943ee
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2016-04-10 21:48:45 +0200

    fixed yuv2rgb convertion, added integer based implementation

--- source/dcv/imgproc/color.d
+++ source/dcv/imgproc/color.d
@@ -324,45 +324,60 @@ unittest {
  * where quantity of luma and chroma values are the same (as in
  * YUV444 format).
  */
-Slice!(3, R*) rgb2yuv(V, R = V)(Slice!(3, V*) range, 
-	Slice!(3, R*) prealloc = emptySlice!(3, R)) {
+Slice!(3, V*) rgb2yuv(V)(Slice!(3, V*) range, 
+	Slice!(3, V*) prealloc = emptySlice!(3, V)) {
 
 	enforce(range.length!2 == 3, "Invalid channel count.");
 
 	if (prealloc.empty || prealloc.shape[].equal(range.shape[])) {
-		prealloc = uninitializedArray!(R[])(range.length!0*range.length!1*3)
+		prealloc = uninitializedArray!(V[])(range.length!0*range.length!1*3)
 			.sliced(range.length!0, range.length!1, 3);
 	}
 
-	foreach(rgb, yuv; lockstep(range.pack!1, prealloc.pack!1)) {
-		auto r = cast(int)rgb[0];
-		auto g = cast(int)rgb[1];
-		auto b = cast(int)rgb[2];
-		yuv[0] = clip!R(r *  .299000 + g *  .587000 + b *  .114000);
-		yuv[1] = clip!R(r * -.168736 + g * -.331264 + b *  .500000 + 128);
-		yuv[2] = clip!R(r *  .500000 + g * -.418688 + b * -.081312 + 128);	
+	foreach(rgb, yuv; lockstep(range.pack!1.byElement, prealloc.pack!1.byElement)) {
+		static if (isFloatingPoint!V) {
+			auto r = cast(int)rgb[0];
+			auto g = cast(int)rgb[1];
+			auto b = cast(int)rgb[2];
+			yuv[0] = clip!V((r * .257) + (g * .504) + (b * .098) + 16);
+			yuv[1] = clip!V((r * .439) + (g * .368) + (b * .071) + 128);
+			yuv[2] = clip!V(-(r * .148) - (g * .291) + (b * .439) + 128);	
+		} else {
+			auto r = rgb[0];
+			auto g = rgb[1];
+			auto b = rgb[2];
+			yuv[0] = clip!V(( (  66 * (r) + 129 * (g) +  25 * (b) + 128) >> 8) +  16);
+			yuv[1] = clip!V(( ( -38 * (r) -  74 * (g) + 112 * (b) + 128) >> 8) + 128);
+			yuv[2] = clip!V(( ( 112 * (r) -  94 * (g) -  18 * (b) + 128) >> 8) + 128);
+		}
 	}
 
 	return prealloc;
 }
 
-Slice!(3, R*) yuv2rgb(V, R = V)(Slice!(3, V*) range, 
-	Slice!(3, R*) prealloc = emptySlice!(3, R)) {
+Slice!(3, V*) yuv2rgb(V)(Slice!(3, V*) range, 
+	Slice!(3, V*) prealloc = emptySlice!(3, V)) {
 
 	enforce(range.length!2 == 3, "Invalid channel count.");
 
 	if (prealloc.empty || prealloc.shape[].equal(range.shape[])) {
-		prealloc = uninitializedArray!(R[])(range.length!0*range.length!1*3)
+		prealloc = uninitializedArray!(V[])(range.length!0*range.length!1*3)
 			.sliced(range.length!0, range.length!1, 3);
 	}
 
 	foreach(yuv, rgb; lockstep(range.pack!1.byElement, prealloc.pack!1.byElement)) {
-		auto y = cast(int)(yuv[0]);
+		auto y = cast(int)(yuv[0]) - 16;
 		auto u = cast(int)(yuv[1]) - 128;
 		auto v = cast(int)(yuv[2]) - 128;
-		rgb[0] = clip!R(y + 1.4075 * v);
-		rgb[1] = clip!R(y - 0.3455 * u - (0.7169 * v));
-		rgb[2] = clip!R(y + 1.7790 * u);
+		static if (isFloatingPoint!V) {
+			rgb[0] = clip!V(y + 1.4075 * v);
+			rgb[1] = clip!V(y - 0.3455 * u - (0.7169 * v));
+			rgb[2] = clip!V(y + 1.7790 * u);
+		} else {
+			rgb[0] = clip!V(( 298 * y + 409 * v + 128) >> 8);
+			rgb[1] = clip!V(( 298 * y - 100 * u - 208 * v + 128) >> 8);
+			rgb[2] = clip!V(( 298 * y + 516 * u + 128) >> 8);
+		}
 	}
 
 	return prealloc;
