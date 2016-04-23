a705846d219b89cc17454331173b130452addb80
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2016-04-23 23:59:38 +0200

    Aded feature detector base API interface

---
+++ source/dcv/features/detector/package.d
@@ -0,0 +1,18 @@
+﻿module dcv.features.detector;
+
+import std.experimental.ndslice;
+
+public import dcv.core.image : Image;
+public import dcv.features.utils : Feature;
+
+/**
+ * Feature detector interface.
+ * 
+ * Each feature detector algorithm
+ * implements this interface.
+ */
+interface Detector
+{
+	Feature [] detect(in Image image, size_t count) const;
+}
+
