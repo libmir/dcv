925d0a95aa38181b29fd8c97a876fb108f7b5617
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2016-06-18 10:36:50 +0200

    Converted content to tree view

--- javascripts/ddocs.js
+++ javascripts/ddocs.js
@@ -29,20 +29,25 @@ var docs = [
     "dcv_tracking_opticalflow_base.html", 
 ];
 
+
 var examples = [
     "example_filter.html",
     "example_features.html",
     "example_video.html",
+    "example_imgmanip.html",
     "example_tracking_hornschunck.html",
-    "example_tracking_lucaskanade.html",
-    "example_imgmanip.html"
+    "example_tracking_lucaskanade.html"
 ];
 
 var DESKTOP_UI = 0;
 var PHONE_UI = 1;
 var DESKTOP_MIN_WIDTH = 1200;
-var CONTENT_SYMB_UP = 'Content ▲';
-var CONTENT_SYMB_DOWN = 'Content ▼';
+//var TREE_SYMB_UP = '▲';
+//var TREE_SYMB_DOWN = '▼';
+var TREE_SYMB_UP = '◎';
+var TREE_SYMB_DOWN = '◉';
+var CONTENT_SYMB_UP = TREE_SYMB_UP + ' Content';
+var CONTENT_SYMB_DOWN =  TREE_SYMB_DOWN + ' Content';
 
 var PROJECT_BOX_WIDTH = "88%"
 var PROJECT_BOX_WIDTH_PHONE = "60%";
@@ -64,7 +69,6 @@ function hideContent() {
                 );
     }
     $("#sourcetreebox").hide(animationSpeed);
-
 }
 
 function showContent() {
@@ -79,7 +83,7 @@ function showContent() {
 }
 
 function toggleContent() {
-    $("#sourcetree").children('li').toggle(animationSpeed);
+    $("#sourcetree").children().toggle(animationSpeed);
     if ($("#content").text() == CONTENT_SYMB_UP) {
         $("#content").text(CONTENT_SYMB_DOWN);
     } else {
@@ -107,7 +111,108 @@ function loadHTML(path) {
     });
 }
 
+function expandTreeItem(id) {
+
+}
+
+function collapseTreeItem(id) {
+
+}
+
+function isTreeItemCollapsed(id) {
+
+}
+
+function toggleTreeItem(id) {
+    if (isTreeItemCollapsed(id))
+        expandTreeItem(id);
+    else
+        collapseTreeItem(id);
+}
+
 function setupContent(content) {
+
+    var sourcetree = document.getElementById("sourcetree");
+    sourcetree.innerHTML = '';
+
+    if (content.length == 0) {
+        hideContent();
+        return;
+    } 
+
+    content.forEach(function(item, index) {
+        tokens = item.replace(".html", "").split("_");
+
+        var ul = null;
+        var parentUl = document.getElementById("sourcetree");
+
+        var uniquePath = "";
+
+        for (j = 0; j < tokens.length - 1; ++j) {
+
+            if (j > 0) {
+                uniquePath += "_";
+            }
+
+            uniquePath += tokens[j];
+
+            ul = document.getElementById(uniquePath);
+
+            if (ul == null) {
+                var li= document.createElement("li");
+
+                li.innerHTML = '<a style="outline:none" href="javascript:;">' + TREE_SYMB_DOWN + " " + tokens[j] + '</a>';
+                li.id = uniquePath + "_root";
+                $(li).css("list-style-type", "none");
+                $(li).css("cursor", "pointer");
+
+                ul = document.createElement("ul");
+                ul.id = uniquePath;
+
+                parentUl.appendChild(li);
+                parentUl.appendChild(ul);
+
+                // setup onclick toggle animation
+                li.onclick = function(uniquePath) { return function() {
+                    var text = $(this).text();
+                    if (text.indexOf(TREE_SYMB_DOWN) != -1) {
+                        // it is expanded
+                        $(this).text(text.replace(TREE_SYMB_DOWN, TREE_SYMB_UP));
+                    } else {
+                        $(this).text(text.replace(TREE_SYMB_UP, TREE_SYMB_DOWN));
+                    }
+                    var ul = document.getElementById(uniquePath);
+                    $(ul).children().toggle(animationSpeed);
+                }; }(uniquePath);
+            }
+            parentUl = ul;
+        }
+
+        var li = document.createElement("li");
+        li.id = item;
+
+        var lis = "<a href=\"javascript:;\">";
+        lis += tokens[tokens.length-1];
+        lis += "</a>\n";
+
+        li.innerHTML = lis;
+
+        var htmlLoadFunc = function() {
+            setLocationQueryString(item);
+        };
+
+        li.onclick = htmlLoadFunc;
+        $(li).css("list-style-type", "circle");
+
+        parentUl.appendChild(li);
+    });
+
+    showContent();
+}
+
+/*
+function setupContent(content) {
+
     var sourcetree = document.getElementById("sourcetree");
     sourcetree.innerHTML = '';
 
@@ -130,16 +235,6 @@ function setupContent(content) {
         var li = document.createElement("li");
         li.innerHTML = lis;
         var htmlLoadFunc = function() {
-            /*
-            var html = $.ajax({
-                url : item,
-                dataType : 'text',
-                success: function(result) {
-                    document.getElementById("pagemain").innerHTML = result;
-                }
-            });
-            */
-            //loadHTML(item);
             setLocationQueryString(item);
         };
         li.onclick = htmlLoadFunc;
@@ -147,9 +242,19 @@ function setupContent(content) {
     });
     showContent();
 }
+*/
 
 function setupDocumentation() {
     setupContent(docs);
+    if (currentUI == PHONE_UI) {
+        // collapse sub-packages of dcv
+        document.getElementById("dcv_core_root").click();
+        document.getElementById("dcv_features_root").click();
+        document.getElementById("dcv_imgproc_root").click();
+        document.getElementById("dcv_io_root").click();
+        document.getElementById("dcv_plot_root").click();
+        document.getElementById("dcv_tracking_root").click();
+    }
 }
 
 function setupExamples() {
@@ -158,20 +263,42 @@ function setupExamples() {
 
 function setupPhoneUI() {
     animationSpeed = 0;
+
+    document.getElementById('sourcetreeboxphone').appendChild(document.getElementById('sourcetreebox'));
+
     $("#sourcetree").children('li').slideUp(animationSpeed);
-    $(".roundedbox").css("border-radius", "0px");
-    $("#sourcetreebox").css("width", $("#projectbox").css("width"));
+    $("#menu").css("margin-bottom", "15px");
+
+    $(".roundedbox").css("background", "none");
+    $(".roundedbox").css("border", "0px");
+    $(".roundedbox").css("padding-top", "0px");
+    $(".roundedbox").css("padding-left", "0px");
+    $(".roundedbox").css("margin-top", "0px");
+    $(".roundedbox").css("margin-left", "0px");
+
     $("#sourcetreebox").css('min-height', '0');
+    $("#sourcetreebox").css('width', '98%');
+    $("#sourcetreebox").css('padding-left', '10px');
+
     $('#return-to-top').hide();
     hideContent();
 }
 
 function setupDesktopUI() {
     animationSpeed = 200;
+
+    document.getElementById('sourcetreeboxdesktop').appendChild(document.getElementById('sourcetreebox'));
+
     $(".roundedbox").css("border-radius", "16px");
+    $(".roundedbox").css("background", "#f4f4f4");
+    $(".roundedbox").css("border", "1px solid #e0e0e0");
+    $(".roundedbox").css("padding", "15px");
+    $(".roundedbox").css("margin", "10px");
+
     $("#sourcetreebox").css("width", "270px");
     $("#sourcetreebox").css('min-height', '500px');
     $("#projectbox").css("width", PROJECT_BOX_WIDTH);
+
     hideContent();
 }
 
