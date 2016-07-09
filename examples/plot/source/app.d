4b3ce898872dd147ce31af312e120f5556ef674c
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2016-07-10 00:21:28 +0200

    Example cleanup

--- examples/plot/source/app.d
+++ examples/plot/source/app.d
@@ -9,26 +9,8 @@ import std.stdio;
 import dcv.core;
 import dcv.io;
 import dcv.imgproc;
-
-import core.stdc.stdlib;
-import core.thread;
-
 import dcv.plot.figure;
 
-auto assignTextureSize(int _width, int _height)
-{
-    int w = 2;
-    int h = 2;
-
-    while(w < _width) {
-        w  = w^^2;
-        writeln(w);
-    }
-    while(h < _height) {
-        h = h^^2;
-    }
-    return [w, h];
-}
 
 void main(string [] args) {
 
