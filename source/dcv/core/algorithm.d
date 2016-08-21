6fb6e43b7018ef2d54bcc62a94fa1c99a75a0dfc
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2016-08-21 08:35:54 +0200

    Refactoring - utilized mir.ndslice.algorithm.

--- source/dcv/core/algorithm.d
+++ source/dcv/core/algorithm.d
@@ -22,10 +22,15 @@ License: $(LINK3 http://www.boost.org/LICENSE_1_0.txt, Boost Software License - 
 module dcv.core.algorithm;
 
 import std.range;
-import std.traits : isNumeric, isAssignable;
-import std.algorithm : map, each, max, min, reduce;
+import std.traits : isNumeric, isFloatingPoint;
 
-import mir.ndslice;
+import mir.ndslice.slice : Slice, sliced, DeepElementType;
+import mir.ndslice.algorithm : ndFold, ndReduce, ndEach, Yes;
+
+version(unittest)
+{
+    import std.math : sqrt, approxEqual;
+}
 
 /**
 Type of matrix and vector norms.
@@ -37,252 +42,245 @@ enum NormType
     L2 /// Eucledian norm, sqrt(sum(x^^2))
 }
 
-private ElementType!Range findMinMax(string comparator, Range)(Range range) pure nothrow 
-        if (isForwardRange!Range && isNumeric!(ElementType!Range))
-{
-    ElementType!Range v = range.front;
-    mixin("foreach(e; range) { if (e " ~ comparator ~ " v) v = e; }");
-    return v;
-}
-
-/**
-Find minimum value in the forward range.
-
-Params:
-    range = Forward range in which minimum value is looked for.
-
-Returns:
-    Minimum value of all elements in given range.
-*/
-ElementType!Range findMin(Range)(Range range) pure nothrow 
-        if (isForwardRange!Range && isNumeric!(ElementType!Range))
-{
-    return range.findMinMax!"<";
-}
-
-/// Find minimal value in the array.
-unittest
-{
-    auto arr = [1, 2, 3];
-    assert(arr.findMin == 1);
-}
-
-/**
-Find maximum value in the forward range.
-
-Params:
-    range = Forward range in which maximum value is looked for.
-
-Returns:
-    Maximum value of all elements in given range.
-*/
-ElementType!Range findMax(Range)(Range range) pure nothrow 
-        if (isForwardRange!Range && isNumeric!(ElementType!Range))
-{
-    return range.findMinMax!">";
-}
-
-/// Find maximal value in the array.
-unittest
-{
-    auto arr = [1, 2, 3];
-    assert(arr.findMax == 3);
-}
-
 /**
 Calculate value of various norm types for vectors and matrices.
 
 Params:
-    range = Forward range that represents vector or matrix of which the norm is calculated.
+    tensor = Tensor of which the norm value is calculated.
     normType = requested type of norm.
 
 Returns:
-Calculated norm value as real.
+    Calculated norm value as real.
 */
-real norm(Range)(Range range, in NormType normType)
-        if (isForwardRange!Range && isNumeric!(ElementType!Range)
-            && isAssignable!(real, ElementType!Range))
+@nogc pure nothrow real norm(Range, size_t N)(auto ref Slice!(N, Range) tensor, NormType normType)
 {
+    import std.algorithm.comparison : max;
+    import std.conv : to;
+
+    version (LDC)
+        import ldc.intrinsics : sqrt = llvm_sqrt, abs = llvm_abs;
+    else
+        import std.math : sqrt, abs;
+
+    alias T = DeepElementType!(typeof(tensor));
+
+    static if (isFloatingPoint!T)
+        auto min = T.min_normal;
+    else
+        auto min = T.min;
+
     final switch (normType)
     {
     case NormType.INF:
-        return range.normImpl_inf;
+        return min.ndReduce!max(tensor).to!real;
     case NormType.L1:
-        return range.normImpl_n1;
+        return T(0).ndReduce!((a, b) => abs(a + b))(tensor).to!real;
     case NormType.L2:
-        return range.normImpl_n2;
+        return T(0).ndReduce!((a, b) => a + b ^^ 2)(tensor).to!real.sqrt;
     }
 }
 
-unittest
+// Test INF norm
+pure nothrow unittest
 {
-    import std.math : sqrt, approxEqual;
+    import std.conv : to;
 
-    auto arr = [1.0f, 2.0f, 3.0f];
-    assert(approxEqual(arr.norm(NormType.INF), 3.0f));
-    assert(approxEqual(arr.norm(NormType.L1), ((arr[0] + arr[1] + arr[2]))));
-    assert(approxEqual(arr.norm(NormType.L2), sqrt(arr[0] ^^ 2 + arr[1] ^^ 2 + arr[2] ^^ 2)));
+    auto t1 = tensor1();
+    auto t2 = tensor2();
+    auto t3 = tensor3();
+
+    assert(approxEqual(t1.norm(NormType.INF), 3.0f));
+    assert(approxEqual(t2.norm(NormType.INF), 9.0f));
+    assert(approxEqual(t3.norm(NormType.INF), 27.0f));
+}
+
+// Test L1 norm - test vector sum == 1 after normalization
+pure nothrow unittest
+{
+    auto t1 = tensor1();
+    auto t2 = tensor2();
+    auto t3 = tensor3();
+
+    t1[] /= t1.norm(NormType.L1);
+    t2[] /= t2.norm(NormType.L1);
+    t3[] /= t3.norm(NormType.L1);
+
+    assert(approxEqual(t1.ndFold!"a+b"(0.0f), 1.0f));
+    assert(approxEqual(t2.ndFold!"a+b"(0.0f), 1.0f));
+    assert(approxEqual(t3.ndFold!"a+b"(0.0f), 1.0f));
+}
+
+// Test L2 norm - test vector length == 1 after normalization
+pure nothrow unittest
+{
+    auto t1 = tensor1();
+    auto t2 = tensor2();
+    auto t3 = tensor3();
+
+    t1[] /= t1.norm(NormType.L2);
+    t2[] /= t2.norm(NormType.L2);
+    t3[] /= t3.norm(NormType.L2);
+
+    assert(approxEqual(t1.ndFold!( (v, n) => v + n*n)(0.0f), 1.0f));
+    assert(approxEqual(t2.ndFold!( (v, n) => v + n*n)(0.0f), 1.0f));
+    assert(approxEqual(t3.ndFold!( (v, n) => v + n*n)(0.0f), 1.0f));
 }
 
 /**
-Normalize range using given norm type.
-
-Performs lazy normalization by utilizing std.algorithm.iteration.map.
+Normalize tensor values using given norm type.
 
 Params:
-    range = Forward range that represent vector or matrix which is normalized.
-    normType = requested norm type for normalization.
+    tensor = Tensor which is normalized.
+    normType = Requested norm type for normalization.
 
 Returns:
-    Returns the map result range that evaluates normalization lazily.
+    Returns normalized input tensor.
 */
-auto normalize(Range)(Range range, NormType normType = NormType.L2)
-        if (isForwardRange!Range && isAssignable!(real, ElementType!Range))
+@nogc nothrow auto normalized(Range, size_t N)(auto ref Slice!(N, Range) tensor, NormType normType = NormType.L2)
 {
-    // TODO: redesign as pure
-    auto n = range.norm(normType);
-    return range.map!(v => v / n);
-}
-
-unittest
-{
-    import std.algorithm.comparison : equal;
-    import std.math : sqrt, approxEqual;
-
-    auto arr = [0.0, 1.0, 2.0];
-    auto infNorm = 2.0;
-    auto l1Norm = 3.0;
-    auto l2Norm = sqrt(5.0);
-    assert(equal!approxEqual([arr[0] / infNorm, arr[1] / infNorm,
-            arr[2] / infNorm], arr.normalize(NormType.INF).array));
-    assert(equal!approxEqual([arr[0] / l1Norm, arr[1] / l1Norm, arr[2] / l1Norm],
-            arr.normalize(NormType.L1).array));
-    assert(equal!approxEqual([arr[0] / l2Norm, arr[1] / l2Norm, arr[2] / l2Norm],
-            arr.normalize(NormType.L2).array));
-}
-
-/**
-Scale range values by shift and multiplication.
-
-Performs value scaling by multiplication and shifting by following equation:
-$(D_CODE $(D_PARAM out) = $(D_PARAM alpha)*$(D_PARAM range) + $(D_PARAM beta))
-
-Performs lazy scale of the values by utilizing std.algorithm.iteration.map.
-
-Params:
-    range = Forward range in which values are being scaled.
-    alpha = Multiplier value.
-    beta = Shift value.
-
-Returns:
-    Returns the map result range that evaluates value scaling lazily.
-*/
-auto scaled(Scalar, Range)(Range range, Scalar alpha = 1, Scalar beta = 0) pure @safe nothrow 
-        if (isForwardRange!Range && isNumeric!(ElementType!Range)
-            && isNumeric!Scalar && isAssignable!(ElementType!Range, Scalar))
-{
-    static if (is(ElementType!Range == Scalar))
-        return range.map!(v => alpha * (v) + beta);
+    alias T = DeepElementType!(typeof(tensor));
+    auto n = tensor.norm(normType);
+    static if (isFloatingPoint!T)
+        tensor[] /= n;
     else
-        return range.map!(v => cast(ElementType!Range)(alpha * (v) + beta));
+        tensor.ndEach!((ref v) { v = cast(T)(cast(real)v / n); }, Yes.vectorized);
+    return tensor;
 }
 
-///
-unittest
+nothrow unittest
 {
-    auto arr = [0.0, 0.5, 1.0];
-    arr = arr.scaled(10.0, 2.0).array;
-    assert(arr[0] == 2.0);
-    assert(arr[1] == 7.0);
-    assert(arr[2] == 12.0);
+    auto t = tensor1();
+    auto tn = normalized(t, NormType.L1);
+    assert(t.ptr == tn.ptr);
 }
 
 /**
-Scale range values to fit a given value range.
-
-Performs lazy scale of the values by utilizing std.algorithm.iteration.map.
+Scale tensor values.
 
 Params:
-    range = Forward range in which values are being scaled.
-    min = Minimal range value.
-    max = Maximal range value.
+    tensor = Input tensor.
+    alpha = Multiplier value.
+    beta = Offset value.
+
+Performs value modification of tensor elements using following formula:
+----
+out = alpha * (in) + beta;
+----
 
 Returns:
-    Returns the map result range that evaluates value scaling lazily.
+    Scaled input tensor.
+    
 */
-auto ranged(Scalar, Range)(Range range, Scalar min = 0, Scalar max = 1)
-        if (isForwardRange!Range && isNumeric!(ElementType!Range)
-            && isNumeric!Scalar && isAssignable!(ElementType!Range, Scalar))
+@nogc nothrow auto scaled(Scalar, Range, size_t N)(auto ref Slice!(N, Range) tensor, Scalar alpha = 1, Scalar beta = 0)
+        if (isNumeric!Scalar)
+{
+    tensor.ndEach!((ref v) { v = alpha * (v) + beta; }, Yes.vectorized);
+    return tensor;
+}
+
+nothrow unittest
+{
+    auto t = tensor1();
+    auto ts = t.scaled(10.0f, 2.0f);
+
+    assert(t.ptr == ts.ptr);
+
+    assert(approxEqual(ts[0], 12.0f));
+    assert(approxEqual(ts[1], 22.0f));
+    assert(approxEqual(ts[2], 32.0f));
+}
+
+/**
+In-place tensor scaling to fit given value range.
+
+Params:
+    tensor = Input tensor.
+    minValue = Minimal value output tensor should contain.
+    maxValue = Maximal value output tensor should contain.
+
+*/
+@nogc auto ranged(Scalar, Range, size_t N)(auto ref Slice!(N, Range) tensor,
+        Scalar minValue = 0, Scalar maxValue = 1) if (isNumeric!Scalar)
 {
     import std.traits : isFloatingPoint;
+    import std.algorithm.comparison : min, max;
 
-    auto _min = range.findMin;
-    auto _max = range.findMax;
-    auto _d = _max - _min;
-    static if (isFloatingPoint!Scalar)
-    {
-        auto sc_val = (max / _d);
-        static if (is(ElementType!Range == Scalar))
-            return range.map!(a => (a - _min) * sc_val);
-        else
-            return range.map!(a => cast(ElementType!Range)((a - _min) * sc_val));
-    }
+    alias RangeElementType = DeepElementType!(typeof(tensor));
+
+    auto _min = tensor.ndFold!min(RangeElementType.max);
+    static if (isFloatingPoint!RangeElementType)
+        auto _max = tensor.ndFold!max(RangeElementType.min_normal);
     else
+        auto _max = tensor.ndFold!max(RangeElementType.min);
+
+    auto rn_val = _max - _min;
+    auto sc_val = maxValue - minValue;
+
+    tensor.ndEach!((ref a) { a = sc_val * ((a - _min) / rn_val) + minValue; }, Yes.vectorized);
+
+    return tensor;
+}
+
+unittest
+{
+    immutable smin = -10.0f;
+    immutable smax = 15.0f;
+
+    auto t1 = tensor1();
+    auto t2 = tensor2();
+    auto t3 = tensor3();
+
+    auto t1r = t1.ranged(smin, smax);
+    auto t2r = t2.ranged(smin, smax);
+    auto t3r = t3.ranged(smin, smax);
+
+    assert(t1.ptr == t1r.ptr);
+    assert(t2.ptr == t2r.ptr);
+    assert(t3.ptr == t3r.ptr);
+
+}
+
+nothrow unittest
+{
+    import std.algorithm.comparison : max, min;
+
+    immutable smin = -10.0f;
+    immutable smax = 15.0f;
+
+    auto t1 = tensor1();
+    auto t2 = tensor2();
+    auto t3 = tensor3();
+
+    auto t1r = t1.ranged(smin, smax);
+    auto t2r = t2.ranged(smin, smax);
+    auto t3r = t3.ranged(smin, smax);
+
+    assert(approxEqual(ndFold!min(t1r, float.max), smin));
+    assert(approxEqual(ndFold!max(t1r, float.min_normal), smax));
+
+    assert(approxEqual(ndFold!min(t2r, float.max), smin));
+    assert(approxEqual(ndFold!max(t2r, float.min_normal), smax));
+
+    assert(approxEqual(ndFold!min(t3r, float.max), smin));
+    assert(approxEqual(ndFold!max(t3r, float.min_normal), smax));
+}
+
+version (unittest)
+{
+    auto tensor1()
     {
-        auto sc_val = cast(float)(max / _d);
-        return range.map!(a => cast(ElementType!Range)(cast(float)(a - _min) * sc_val));
+        return [1.0f, 2.0f, 3.0f].sliced(3);
     }
-}
 
-///
-unittest
-{
-    auto arr = [0.0, 0.5, 1.0].ranged(0, 10).array;
-    assert(arr[0] == 0.);
-    assert(arr[1] == 5.);
-    assert(arr[2] == 10.);
-}
-
-unittest
-{
-    auto arr = [0.0, 0.5, 1.0].ranged(0.0, 10.0).array;
-    assert(arr[0] == 0.);
-    assert(arr[1] == 5.);
-    assert(arr[2] == 10.);
-}
-
-unittest
-{
-    auto arr = [0.0, 0.5, 1.0].ranged(0.0f, 10.0f).array;
-    assert(arr[0] == 0.);
-    assert(arr[1] == 5.);
-    assert(arr[2] == 10.);
-}
-
-private: // implementation
-
-// basic norm implementation ////////
-
-auto normImpl_inf(Range)(Range range)
-{
-    return range.findMax;
-}
-
-auto normImpl_n1(Range)(Range range)
-{
-    import std.math : abs;
-
-    return range.reduce!((a, b) => abs(a + b));
-}
-
-auto normImpl_n2(Range)(Range range)
-{
-    import std.math : sqrt;
-
-    ElementType!Range v = 0;
-    foreach (e; range)
+    auto tensor2()
     {
-        v += e ^^ 2;
+        return [1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f].sliced(3, 3);
     }
-    return sqrt(v);
+
+    auto tensor3()
+    {
+        return [1.0f, 2.0f, 3.0f,       4.0f, 5.0f, 6.0f,       7.0f, 8.0f, 9.0f, 
+               10.0f, 11.0f, 12.0f,     13.0f, 14.0f, 15.0f,    16.0f, 17.0f, 18.0f, 
+               19.0f, 20.0f, 21.0f,     22.0f, 23.0f, 24.0f,    25.0f, 26.0f, 27.0f].sliced(3, 3, 3);
+    }
 }
