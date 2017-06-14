f7f9dca8b2a2748c841da875f487920ff65110b6
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2017-06-14 22:19:15 +0200

    Fixed ld path.

--- scripts/travis_lib_setup.sh
+++ scripts/travis_lib_setup.sh
@@ -6,6 +6,6 @@ CACHE_ARCHIVE="dcv.tar.gz"
 cd ${HOME}
 
 wget -O ${CACHE_ARCHIVE} https://drive.google.com/uc?id=0ByTt1Q1eZW5WVk50MS1sWU9wYTg&export=download
-tar -zxf ${CACHE_ARCHIVE} -C ${LD_LIBRARY_PATH}/
+tar -zxf ${CACHE_ARCHIVE} -C ${LD_LIBRARY_PATH//:}/
 
 cd ${CURR_DIR}
