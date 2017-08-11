c5b0e238e43d0792b0097929737d570e171b159e
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2017-08-11 06:44:49 +0200

    Added decoding operation.

--- source/dcv/io/image.d
+++ source/dcv/io/image.d
@@ -10,7 +10,7 @@
 module dcv.io.image;
 
 import std.exception: enforce;
-import std.range:array;
+import std.range:array, empty;
 import std.algorithm: reduce;
 import std.string: toLower;
 import std.path:extension;
@@ -35,6 +35,15 @@ class IOImageException : Exception
     }
 }
 
+class InvalidDecoderException : Exception
+{
+    this(in string msg = "")
+    {
+        super("Invalid algorithm is used to decode an image" ~ msg.empty ? "" : (", " ~ msg));
+    }
+}
+
+
 /**
    Read image from the file system.
 
@@ -53,17 +62,43 @@ if (isPixel!T)
     return imreadImpl_imageformats!T(path);
 }
 
+enum Codec
+{
+    bmp,
+    png,
+    tga,
+    jpeg
+}
+
+/**
+   Decode raw image data from memory.
+
+   Params:
+      data = Data buffer containing encoded image.
+      codec = Codec algorithm with which the image gets decoded.
+
+   Return:
+      Decoded image.
+
+   Throws:
+     InvalidDecoderException.
+ */
+ContiguousMatrix!T imdecode(T)(in ubyte[] data, Codec codec)
+if (isPixel!T)
+{
+    return imdecodeImpl_imageformats!T(data, codec);
+}
+
 
 /**
    Write image to the given path on the filesystem.
 
    Params:
+      image = Image data.
       path = Path where the image will be written.
-      width = Width of the image.
-      height = Height of the image.
-      format = Format of the image.
-      depth = Bit depth of the image.
-      data = Image data in unsigned bytes.
+
+   Throws:
+      IOImageException.
  */
 void imwrite(SliceKind kind, size_t[] packs, Iterator)
 (
@@ -142,14 +177,81 @@ ContiguousMatrix!T imreadImpl_imageformats(T)(in string path)
         }
     }
     // Wrap third party custom exception type into DCV error type,
-    // Because the underlying library (imageformats) can be changed
+    // The underlying library (imageformats) is likey to be changed
     // someday.
     catch (Throwable e)
     {
         throw new IOImageException(e.msg);
     }
-
 }
+
+ContiguousMatrix!T imdecodeImpl_imageformats(T)(in ubyte[] data, Codec codec)
+{
+    enum ifTypeCode = imreadImpl_imageformats_adoptFormat!T;
+    enum depth      = pixelDepth!T;
+
+    try
+    {
+        static if (depth == 1)
+        {
+            auto    decoder_func = selectDecoder8_imageformats(codec);
+            IFImage ifim         = decoder_func(data, 0);
+            return (cast(T*)ifim.pixels.ptr).sliced(ifim.h, ifim.w);
+        }
+        else static if (depth == 2)
+        {
+            auto      decoder_func = selectDecoder16_imageformats(codec);
+            IFImage16 ifim         = decoder_func(data, 0);
+            return (cast(T*)ifim.pixels.ptr).sliced(ifim.h, ifim.w);
+        }
+        else
+        {
+            static assert(0, "Reading image depth not supported.");
+        }
+    }
+    catch (InvalidDecoderException e)
+    {
+        // just rethrow
+        throw e;
+    }
+    // Wrap third party custom exception type into DCV error type,
+    // The underlying library (imageformats) is likey to be changed
+    // someday.
+    catch (Throwable e)
+    {
+        throw new InvalidDecoderException(e.msg);
+    }
+}
+
+alias DecoderFunc8  = IFImage function(in ubyte[], long);
+alias DecoderFunc16 = IFImage16 function(in ubyte[], long);
+
+DecoderFunc8 selectDecoder8_imageformats(Codec codec)
+{
+    final switch (codec)
+    {
+    case Codec.bmp:
+        return &read_bmp_from_mem;
+    case codec.png:
+        return &read_png_from_mem;
+    case codec.tga:
+        return &read_tga_from_mem;
+    case codec.jpeg:
+        return &read_jpeg_from_mem;
+    }
+}
+
+DecoderFunc16 selectDecoder16_imageformats(Codec codec)
+{
+    switch (codec)
+    {
+    case codec.png:
+        return &read_png16_from_mem;
+    default:
+        throw new InvalidDecoderException("Only png format supported for 16-bit images.");
+    }
+}
+
 
 ColFmt imreadImpl_imageformats_adoptFormat(T)()
 {
@@ -180,5 +282,5 @@ ColFmt imreadImpl_imageformats_adoptFormat(T)()
     {
         static assert(0, "Given pixel type representative is invalid.");
     }
-};
+}
 
