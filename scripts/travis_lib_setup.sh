cb8db274c9417285393fd8be95e4d7715fae233f
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2018-02-18 20:19:46 +0100

    Using package with ffmpeg 3.1.11.

--- scripts/travis_lib_setup.sh
+++ scripts/travis_lib_setup.sh
@@ -6,7 +6,7 @@ CACHE_ARCHIVE="${HOME}/dcv.tar.gz"
 cd ${HOME}
 
 echo Downloading cached archives to ${CACHE_ARCHIVE}
-wget -O ${CACHE_ARCHIVE} "https://drive.google.com/uc?id=0ByTt1Q1eZW5WVk50MS1sWU9wYTg&export=download"
+wget -O ${CACHE_ARCHIVE} "https://drive.google.com/uc?id=1FrZz6W0K1l9gX07lwugUInEdV1Hf6nsu&export=download"
 
 echo Copying to LD_LIBRARY_PATH...
 tar -zxf ${CACHE_ARCHIVE} -C ${LD_LIBRARY_PATH//:}/
